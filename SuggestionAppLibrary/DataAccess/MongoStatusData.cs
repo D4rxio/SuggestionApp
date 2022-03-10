b8e78a102f1df3c6b8578d5f5058be0f275a3e9f
Lukasz Zacharski <zacharski.lukas@gmail.com>
2022-03-10 13:08:57 +0100

    End of Section 10

---
+++ SuggestionAppLibrary/DataAccess/MongoStatusData.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Extensions.Caching.Memory;
+
+namespace SuggestionAppLibrary.DataAccess;
+
+public class MongoStatusData : IStatusData
+{
+   private readonly IMongoCollection<StatusModel> _statuses;
+   private readonly IMemoryCache _cache;
+   private const string CacheName = "StatusCache";
+
+   public MongoStatusData(IDBConnection db, IMemoryCache cache)
+   {
+      _cache = cache;
+      _statuses = db.StatusCollection;
+   }
+
+   public async Task<List<StatusModel>> GetStatuses(string id)
+   {
+      var output = _cache.Get<List<StatusModel>>(CacheName);
+
+      if (output is null)
+      {
+         var results = await _statuses.FindAsync(_ => true);
+         output = results.ToList();
+
+         _cache.Set(CacheName, output, TimeSpan.FromDays(1));
+      }
+
+
+      return output;
+   }
+
+   public Task CreateStatus(StatusModel status)
+   {
+      return _statuses.InsertOneAsync(status);
+   }
+
+
+}
