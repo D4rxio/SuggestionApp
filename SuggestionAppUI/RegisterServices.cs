ea47b700172d1b98e0c8e33e8c8283175f94a1d7
Lukasz Zacharski <zacharski.lukas@gmail.com>
2022-03-26 23:00:40 +0100

    End of lesson 20 - implementing authorization and authencitation to application

--- SuggestionAppUI/RegisterServices.cs
+++ SuggestionAppUI/RegisterServices.cs
@@ -1,21 +1,35 @@
-﻿namespace SuggestionAppUI
+﻿using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.Identity.Web;
+
+namespace SuggestionAppUI;
+
+public static class RegisterServices
 {
-   public static class RegisterServices
+
+   public static void ConfigureServices(this  WebApplicationBuilder builder)
    {
+      builder.Services.AddRazorPages();
+      builder.Services.AddServerSideBlazor();
+      builder.Services.AddMemoryCache();
 
-      public static void ConfigureServices(this  WebApplicationBuilder builder)
+      builder.Services.AddAuthentication(OpenIdConnectDefaults.AuthenticationScheme)
+         .AddMicrosoftIdentityWebApp(builder.Configuration.GetSection("AzureAdB2C"));
+
+      builder.Services.AddAuthorization(options =>
       {
-         builder.Services.AddRazorPages();
-         builder.Services.AddServerSideBlazor();
-         builder.Services.AddMemoryCache();
+         options.AddPolicy("Admin", policy =>
+         {
+            policy.RequireClaim("jobTitle", "Admin");
+         });
 
-         builder.Services.AddSingleton<IDBConnection, DBConnection>();
-         builder.Services.AddTransient<ICategoryData, MongoCategoryData>();
-         builder.Services.AddTransient<IStatusData,MongoStatusData>();
-         builder.Services.AddTransient<ISuggestionData, MongoSuggestionData>();
-         builder.Services.AddTransient<IUserData, MongoUserData>();
+      });
+
+      builder.Services.AddSingleton<IDBConnection, DBConnection>();
+      builder.Services.AddTransient<ICategoryData, MongoCategoryData>();
+      builder.Services.AddTransient<IStatusData,MongoStatusData>();
+      builder.Services.AddTransient<ISuggestionData, MongoSuggestionData>();
+      builder.Services.AddTransient<IUserData, MongoUserData>();
 
 
-      }
    }
 }
